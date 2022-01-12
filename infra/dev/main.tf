a308b8182cc94cbe78b7878abb1f4294481036e3
Alex Gorbachov <agorbachov@gmail.com>
2022-01-12 14:56:47 +0200

    Edited the main.tf file

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -9,4 +9,16 @@ terraform {
     required_version = ">= 0.14.10"
 }
 
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
 
+resource "aws_instance" "Terraform-.....-ec2" {
+  ami = "ami-830....."
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MnistDevBranchInstance"
+  }
+}
