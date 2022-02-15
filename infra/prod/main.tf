6b123119f75c1cc41624c36d48a4a175499f1916
Alex Gorbachov <agorbachov@gmail.com>
2022-02-15 13:59:42 +0200

    Created veriables.tf file in prod

---
+++ infra/prod/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.68"
+    }
+  }
+
+  required_version = ">= 0.14.10"
+}
+
+provider "aws" {
+  profile = "default" 
+  region = "us-east-2"
+}
+
+resource "aws_instance" "Terraform-Mnist-ec2" {
+  ami = "ami-0ed9277fb7eb570c9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MnistDevBranchInstance"
+    Terraform = "True"
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+
+  tags = var.vpc_tags
+}
